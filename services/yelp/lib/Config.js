6fba06c595a83c8db3bf89d6959977c0addc0d7a
Angel Flores <angel.r.flores1@gmail.com>
2014-01-25 16:20:11 -0500

    Added Config.js for yelp service

---
+++ services/yelp/lib/Config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  CONSUMER_KEY: 'PXGP2KNzL7qxRogSiBPLjQ',
+  CONSUMER_SECRET: 'A05nrkWTTPIK0VQtu79xaqP-KGw',
+  TOKEN: 'uwPQ3Hzcxc7YrMHr--00R3NKkD9cjtBi',
+  TOKEN_SECRET: '4e3ldxUIjeSpHYhkgeBoG2VtRVo',
+
+  SEARCH_API_PATH: 'http://api.yelp.com/v2/search'
+};
